8ffcdf02d02ca05907d135e4bf5e0dbb9333444f
Shubham Chandravanshi <shubhamcweb@gmail.com>
2024-11-28 01:25:04 -0600

    footer style correction

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="grow-0 flex flex-col flex-wrap items-center justify-center text-center w-full p-4 border-t border-gray-900 text-sm text-slate-50">
+		<footer className="flex flex-col flex-wrap items-center justify-center text-center w-full p-4 border-t border-gray-900 text-sm text-slate-50">
 			<p className="font-bold mb-2">
 				&copy; {new Date().getFullYear()} UTA Datathon
 			</p>
