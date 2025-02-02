656c9f4b1de8e96c6e70e7409677b20dc573bcf1
SujitShres1ha <sthasujit2062@gmail.com>
2025-02-02 01:41:36 -0600

    Add font styles and animation to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 // import Image from "next/image";
+"use client";
 import React from "react";
 
 export default function Home() {
+  const handleClick = () => {};
   return (
     <div className="video-container">
       <video autoPlay loop muted playsInline className="background-video">
@@ -9,10 +11,21 @@ export default function Home() {
         Your browser does not support the video tag.
       </video>
       <div className="content">
-        <h1>Datathon</h1>
-        
+        <div className="space-y-5 mt-10 font-press-start text-center">
+          <h1 className="animate-glitch text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold text-green-400">
+            UTA DATATHON
+          </h1>
+          <p className="animate-pulse text-green-500 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+            APR 12 - 13
+          </p>
+          <button
+            onClick={handleClick}
+            className="border-2 text-green-500 border-green-500 px-4 py-3 font-bold text-sm sm:text-xl md:text-xl lg:text-2xl hover:text-green-300 hover:border-green-300 hover:border-4 transition-transform transform hover:scale-105 ease-in-out duration-200"
+          >
+            CLICK HERE TO REGISTER
+          </button>
+        </div>
       </div>
     </div>
   );
 }
-
